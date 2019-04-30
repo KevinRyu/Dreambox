8805e97fadad9a943630b1029b07b9f6d0a44b66
KevinRyu <gphy73@gmail.com>
2019-05-01 07:36:09 +0900

    Create weight_map.py
    Visualization of convolution filter for CNN network

---
+++ weight_map.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("wmap.csv")
+data.head()
+
+weights = data.pivot_table("w","y", "x" )
+
+plt.title("Weights map")
+sns.heatmap(weights, annot=True, fmt=".0f", linewidths=0.5, linecolor="Gray")
+plt.show()
